2a39343e8847926b8e0d25195f12a1b1b0c22ac4
bilbo057 <bilbo057@gmail.com>
2024-11-08 12:52:14 +0200

    Changed routing, tested page UI`s and and added additional file for the database connection with the accounts

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+// src/app/services/auth.service.ts
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private afAuth: AngularFireAuth) {}
+
+  async register(email: string, password: string) {
+    return this.afAuth.createUserWithEmailAndPassword(email, password);
+  }
+
+  async login(email: string, password: string) {
+    return this.afAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  async logout() {
+    return this.afAuth.signOut();
+  }
+}
