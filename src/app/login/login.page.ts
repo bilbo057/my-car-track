042bc9cf3a518fb0614aa5537ff4a93d024b243e
bilbo057 <bilbo057@gmail.com>
2024-11-08 12:54:38 +0200

    Added login page working with the accounts from the firestrore

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+  email: string;
+  password: string;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.email = ''; // Initialize email
+    this.password = ''; // Initialize password
+  }
+
+  async login() {
+    try {
+      await this.authService.login(this.email, this.password);
+      this.router.navigate(['/']); // Navigate to the home page after login
+    } catch (error) {
+      console.error("Login failed", error);
+      // Display an error message to the user
+    }
+  }
+}
