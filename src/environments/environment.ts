575f24fb212c5986f5f200743d893c54fd17e4e9
bilbo057 <bilbo057@gmail.com>
2024-11-08 12:49:36 +0200

    Edited the starting files so the app can connect to the firestore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
+// environment.ts
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAPFwx7jBCXlUPJcf7T2mfBl46Tih2eNNM",
+    authDomain: "mycartrack-aab9a.firebaseapp.com",
+    projectId: "mycartrack-aab9a",
+    storageBucket: "mycartrack-aab9a.appspot.com",
+    messagingSenderId: "195120580015",
+    appId: "1:195120580015:web:1b8adf57da7d8634fe5b5a",
+    measurementId: "G-97WS2BFVDE"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
