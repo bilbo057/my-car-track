e3350554d479d2e14d10dc499089bb08ba07c0d6
bilbo057 <bilbo057@gmail.com>
2024-12-12 11:11:56 +0200

    added new sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-// app.module.ts 
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SidebarComponent } from './sidebar/sidebar.component';  // Import the sidebar component
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
@@ -12,7 +13,10 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    SidebarComponent  // Declare the sidebar component
+  ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,13 @@
+// sidebar.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent {
+
+  constructor() { }
+
+}
